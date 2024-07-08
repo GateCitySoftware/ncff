a339436a2cb6d9262701a8b340b04051d3c3ba28
GateCitySoftware <153508559+GateCitySoftware@users.noreply.github.com>
2024-07-07 21:56:51 -0400

    Scaffold Schedule

---
+++ db/migrate/20240708015649_create_schedules.rb
@@ -0,0 +1,10 @@
+class CreateSchedules < ActiveRecord::Migration[7.1]
+  def change
+    create_table :schedules, id: :uuid do |t|
+      t.string :name
+      t.date :date
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_08_014937) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_08_015649) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_08_014937) do
     t.datetime "updated_at", null: false
     t.index ["artist_id"], name: "index_performances_on_artist_id"
     t.index ["stage_id"], name: "index_performances_on_stage_id"
+  end
+
+  create_table "schedules", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "stages", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
