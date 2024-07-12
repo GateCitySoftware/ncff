3e06f6e34fe34234c08d5c997384cc6c38c0e7d3
GateCitySoftware <153508559+GateCitySoftware@users.noreply.github.com>
2024-07-12 15:58:03 -0400

    Run migrations for Vendor, Merchant and Activation

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,54 +10,87 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_708_164_335) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_12_195739) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'artists', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
-    t.string 'name'
-    t.text 'bio'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "activations", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "image"
+    t.string "website"
+    t.json "social_media_links"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'performances', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
-    t.uuid 'stage_id', null: false
-    t.uuid 'artist_id', null: false
-    t.datetime 'start_time'
-    t.datetime 'end_time'
-    t.text 'description'
-    t.boolean 'is_headline'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['artist_id'], name: 'index_performances_on_artist_id'
-    t.index ['stage_id'], name: 'index_performances_on_stage_id'
+  create_table "artists", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'schedules', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
-    t.string 'name'
-    t.date 'date'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "merchants", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "image"
+    t.string "website"
+    t.json "social_media_links"
+    t.string "product_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'stages', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
-    t.string 'name'
-    t.text 'description'
-    t.string 'google_place_id'
-    t.string 'google_place_address'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "performances", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "stage_id", null: false
+    t.uuid "artist_id", null: false
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.text "description"
+    t.boolean "is_headline"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_performances_on_artist_id"
+    t.index ["stage_id"], name: "index_performances_on_stage_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email'
-    t.string 'login_token'
-    t.datetime 'login_token_valid_until'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "schedules", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'performances', 'artists'
-  add_foreign_key 'performances', 'stages'
+  create_table "stages", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "google_place_id"
+    t.string "google_place_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "login_token"
+    t.datetime "login_token_valid_until"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "vendors", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "image"
+    t.string "website"
+    t.json "social_media_links"
+    t.string "category"
+    t.string "cuisine_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "performances", "artists"
+  add_foreign_key "performances", "stages"
 end
