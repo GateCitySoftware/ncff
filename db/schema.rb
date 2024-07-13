72345438de7f3fcbdde84d9029b52895bc3716d7
GateCitySoftware <153508559+GateCitySoftware@users.noreply.github.com>
2024-07-13 13:47:37 -0400

    Run migration for Tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_12_195739) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_13_174604) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,6 +67,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_12_195739) do
     t.text "description"
     t.string "google_place_id"
     t.string "google_place_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
