57cde4794985fb90d4010deedc112f29ce74ffa7
GateCitySoftware <153508559+GateCitySoftware@users.noreply.github.com>
2024-07-14 10:03:28 -0400

    Delete dummy data from Artists controller and index view

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,73 +5,7 @@ class ArtistsController < ApplicationController
 
   # GET /artists or /artists.json
   def index
-    @artist = Artist.first
-
-    @stages = [
-      'Cone Health CityStage',
-      'Lawn Stage',
-      'Old Courthouse Stage',
-      'Van Dyke Stage'
-    ]
-
-    @days = %w[Thursday Friday Saturday]
-    @times = (1..8).to_a.map { |hour| ["#{hour}:00", "#{hour}:30"] }.flatten
-
-    @music_genres = [
-      'Alternative',
-      'Blues',
-      'Classical',
-      'Country',
-      'Electronic',
-      'Folk',
-      'Funk',
-      'Hip Hop',
-      'Indie',
-      'Jazz',
-      'Latin',
-      'Metal',
-      'Pop',
-      'Punk',
-      'R&B',
-      'Reggae',
-      'Rock',
-      'Soul',
-      'World'
-    ]
-
-    @dummy_data = [
-      ['THE WAR & TREATY', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/3-1024x732.png'],
-      ['LOS LONELY BOYS', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/1-1024x732.png'],
-      ['MIPSO', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/2-1024x732.png'],
-      ['RISSI PALMER', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/2-1-1024x683.png'],
-      ['THE PSYCODELICS', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/1-1-1024x683.png'],
-      ['SUSTO', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/Untitled-design-14-1024x683.png'],
-      ['JON MUQ', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/7-1024x614.png'],
-      ['TAE LEWIS', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/18-1024x614.png'],
-      ['BIO RITMO', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/8-1024x614.png'],
-      ['OLIVE KLUG', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/21-1024x614.png'],
-      ['DEMEANOR', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/Untitled-design-11-1024x614.png'],
-      ['SAM FRIBUSH ORGAN TRIO featuring Calvin Napper and Charlie Hunter', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/1-2-1024x614.png'],
-      ['CANDICE IVORY ENSEMBLE', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/5-1024x614.png'],
-      ['ETHNO USA', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/2-2-1024x614.png'],
-      ['ELIAS ALEXANDER', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/22-1024x614.png'],
-      ['HOLLER CHOIR', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/24-1024x614.png'],
-      ['LAKOTA JOHN', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/4-1024x614.png'],
-      ['ABBY HAMILTON', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/6-1024x614.png'],
-      ['SHE RETURNS FROM WAR', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/12-1024x614.png'],
-      ['BLUE RIDGE GIRLS', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/16-1024x614.png'],
-      ['CAIQUE VIDAL & BATUQUE', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/13-1024x614.png'],
-      ['DASHAWN HICKMAN presents SACRED STEEL featuring WENDY HICKMAN', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/10-1024x614.png'],
-      ['OLD HEAVY HANDS', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/Untitled-design-10-1024x614.png'],
-      ['COLIN CUTLER & HOT PEPPER JAM', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/23-1024x614.png'],
-      ['DREW FOUST & THE WHEELHOUSE', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/15-1024x614.png'],
-      ['OXENTE', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/14-1024x614.png'],
-      ['EMANUEL WYNTER', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/Untitled-design-12-1024x614.png'],
-      ['WILD ROOTS', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/Untitled-design-13-1024x614.png'],
-      ['UNHEARD PROJECT', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/11-1024x614.png'],
-      ['ABIGAIL DOWD', 'https://ncfolkfestival.com/wp-content/uploads/2024/06/9-1024x614.png']
-    ]
-    # @artists = Artist.all
+    @artists = Artist.all
   end
 
   # GET /artists/1 or /artists/1.json
--- lib/tasks/seed_real_data.rake
+++ lib/tasks/seed_real_data.rake
@@ -1,7 +1,7 @@
 task seed_real_data: :environment do
   JSON.parse(File.read('lib/assets/draft_schedule.json'))['schedule'].each do |performance|
-    stage = Stage.find_or_create_by(name: performance['stage_name'])
-    artist = Artist.find_or_create_by(name: performance['act'])
+    stage = Stage.find_or_create_by!(name: performance['stage_name'])
+    artist = Artist.find_or_create_by!(name: performance['act'])
 
     start_time = performance['time']
     end_time = performance['end_time']
@@ -10,7 +10,7 @@ task seed_real_data: :environment do
     start_time = DateTime.parse("#{date} #{start_time}")
     end_time = DateTime.parse("#{date} #{end_time}") if end_time.present?
 
-    Performance.find_or_create_by(
+    Performance.find_or_create_by!(
       stage_id: stage.id,
       artist_id: artist.id,
       start_time:,
