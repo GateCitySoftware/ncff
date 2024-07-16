7f7b545bb5a3f8f9ada90840a85b2742e84f4bd4
GateCitySoftware <153508559+GateCitySoftware@users.noreply.github.com>
2024-07-16 11:09:36 -0400

    Run migration for EmbeddedContent

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_16_110809) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_16_150810) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,19 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_16_110809) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.index ["slug"], name: "index_artists_on_slug", unique: true
+  end
+
+  create_table "embedded_contents", force: :cascade do |t|
+    t.string "url"
+    t.string "content_type"
+    t.string "title"
+    t.text "embed_code"
+    t.text "description"
+    t.string "embeddable_type", null: false
+    t.uuid "embeddable_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["embeddable_type", "embeddable_id"], name: "index_embedded_contents_on_embeddable"
   end
 
   create_table "external_links", force: :cascade do |t|
