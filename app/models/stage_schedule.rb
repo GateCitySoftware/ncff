d24244737bad2f5edc1dbeb5c2a1f19c658f91da
GateCitySoftware <153508559+GateCitySoftware@users.noreply.github.com>
2024-07-17 21:17:26 -0400

    Start building StageSchedule model (but may not need it!?)

--- app/models/stage_schedule.rb
+++ app/models/stage_schedule.rb
@@ -1,3 +1,14 @@
 class StageSchedule < ApplicationRecord
   belongs_to :stage
+
+  def self.recreate_all!
+    stage_date_performances = Performance.all.group_by do |p|
+      "#{p.stage.name} - #{p.start_time.to_date.strftime('%A')}"
+    end
+
+    stage_date_performances.each do |name, performances|
+    end
+
+    binding.pry
+  end
 end
