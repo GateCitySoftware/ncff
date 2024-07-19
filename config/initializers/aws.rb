c3ddf25f9db08c48bd510ba9563cdc59ddf789ae
GateCitySoftware <153508559+GateCitySoftware@users.noreply.github.com>
2024-07-19 12:38:01 -0400

    Update config initializer for AWS secrets

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,7 +1,17 @@
-Aws.config.update({
-                    region: Rails.application.credentials.dig(:aws, :region),
-                    credentials: Aws::Credentials.new(
-                      Rails.application.credentials.dig(:aws, :access_key_id),
-                      Rails.application.credentials.dig(:aws, :secret_access_key)
-                    )
-                  })
+if Rails.env.development?
+  Aws.config.update({
+                      region: Rails.application.credentials.dig(:aws, :region),
+                      credentials: Aws::Credentials.new(
+                        Rails.application.credentials.dig(:aws, :access_key_id),
+                        Rails.application.credentials.dig(:aws, :secret_access_key)
+                      )
+                    })
+else
+  Aws.config.update({
+                      region: ENV['AWS_REGION'],
+                      credentials: Aws::Credentials.new(
+                        ENV['AWS_ACCESS_KEY_ID'],
+                        ENV['AWS_SECRET_ACCESS_KEY']
+                      )
+                    })
+end
