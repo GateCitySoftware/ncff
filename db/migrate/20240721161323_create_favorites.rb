a378bbd44ea66e8b811022d7f1bb2e0a74fc616b
GateCitySoftware <153508559+GateCitySoftware@users.noreply.github.com>
2024-07-21 12:48:00 -0400

    Update Favorites migration to fix Users table issue

--- db/migrate/20240721161323_create_favorites.rb
+++ db/migrate/20240721161323_create_favorites.rb
@@ -1,5 +1,20 @@
 class CreateFavorites < ActiveRecord::Migration[7.1]
   def change
+    # NOTE: this is weird having this here but necessary....
+    # users was originally created with a bigint id, but we want to use uuids
+    # I went back and changed the original migration and re-ran everything locally
+    # but I can't do that in the Prod environment because it's already been run
+    # and I don't want to drop the DB and lose all the data I have entered...
+    drop_table :users
+    create_table :users, id: :uuid do |t|
+      t.string :email
+      t.string :login_token
+      t.datetime :login_token_valid_until
+      t.datetime :created_at, null: false
+      t.datetime :updated_at, null: false
+      t.string :role, default: 'attendee', null: false
+    end
+
     create_table :favorites, id: :uuid do |t|
       t.references :user, null: false, foreign_key: true, type: :uuid
       t.references :item, polymorphic: true, null: false, type: :uuid
